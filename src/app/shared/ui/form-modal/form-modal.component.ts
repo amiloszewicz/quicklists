ac826a8726adf3ce96d52d4a7b6533bcace4ec6d
amiloszewicz <miloszewicz.adam@gmail.com>
2023-11-20 17:01:04 +0100

    feat form: modal

---
+++ src/app/shared/ui/form-modal/form-modal.component.ts
@@ -0,0 +1,33 @@
+import { KeyValuePipe } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-modal',
+  standalone: true,
+  imports: [ReactiveFormsModule, KeyValuePipe ],
+  template: `
+  <header>
+    <h1>{{ title }}</h1>
+    <button (click)="close.emit()"></button>
+  </header>
+  <section>
+    <form [formGroup]="formGroup" (ngSubmit)="save.emit(); close.emit()">
+      @for (control of formGroup.controls | keyvalue; track control.key) {
+        <div>
+          <label for="control.key">{{ control.key }}</label>
+          <input type="text" [id]="control.key" [formControlName]="control.key">
+        </div>
+      }
+      <button type="submit">Save</button>
+    </form>
+  </section>
+  `,
+  styles: ``
+})
+export class FormModalComponent {
+  @Input() formGroup!: FormGroup
+  @Input() title!: string;
+  @Output() save = new EventEmitter<void>();
+  @Output() close = new EventEmitter<void>();
+}
