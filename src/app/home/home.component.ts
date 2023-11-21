967d5f3e697872423cbe10f2dc8be131a50930e2
amiloszewicz <miloszewicz.adam@gmail.com>
2023-11-21 12:43:43 +0100

    refactor element: home to add form modal

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,51 @@
-import { Component, signal } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { Checklist } from '../shared/interfaces/checklist';
-import { ModalComponent } from "../shared/ui/modal/modal.component";
+import { FormModalComponent } from '../shared/ui/form-modal/form-modal.component';
+import { ModalComponent } from '../shared/ui/modal/modal.component';
 
 @Component({
-    selector: 'app-home',
-    standalone: true,
-    template: `
+  selector: 'app-home',
+  standalone: true,
+  template: `
     <header>
       <h1>Quicklists</h1>
       <button (click)="checklistsBeingEdited.set({})">Add Checklist</button>
     </header>
 
     <app-modal [IsOpen]="!!checklistsBeingEdited()">
-      <ng-template>You can't see me yet...</ng-template>
+      <ng-template>
+        <app-form-modal
+          [title]="
+            checklistsBeingEdited()?.title
+              ? checklistsBeingEdited()!.title!
+              : 'Add checklist'
+          "
+          [formGroup]="checklistForm"
+          (close)="checklistsBeingEdited.set(null)"
+        ></app-form-modal>
+      </ng-template>
     </app-modal>
   `,
-    styles: ``,
-    imports: [ModalComponent]
+  styles: ``,
+  imports: [ModalComponent, FormModalComponent],
 })
-export default  class HomeComponent {
+export default class HomeComponent {
+  formBuilder = inject(FormBuilder);
+
   checklistsBeingEdited = signal<Partial<Checklist> | null>(null);
+
+  checklistForm = this.formBuilder.nonNullable.group({
+    title: [''],
+  });
+
+  constructor() {
+    effect(() => {
+      const checklist = this.checklistsBeingEdited();
+
+      if (checklist!) {
+        this.checklistForm.reset();
+      }
+    });
+  }
 }
--- src/app/shared/ui/form-modal/form-modal.component.ts
+++ src/app/shared/ui/form-modal/form-modal.component.ts
@@ -5,28 +5,32 @@ import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-form-modal',
   standalone: true,
-  imports: [ReactiveFormsModule, KeyValuePipe ],
+  imports: [ReactiveFormsModule, KeyValuePipe],
   template: `
-  <header>
-    <h1>{{ title }}</h1>
-    <button (click)="close.emit()"></button>
-  </header>
-  <section>
-    <form [formGroup]="formGroup" (ngSubmit)="save.emit(); close.emit()">
-      @for (control of formGroup.controls | keyvalue; track control.key) {
+    <header>
+      <h1>{{ title }}</h1>
+      <button (click)="close.emit()"></button>
+    </header>
+    <section>
+      <form [formGroup]="formGroup" (ngSubmit)="save.emit(); close.emit()">
+        @for (control of formGroup.controls | keyvalue; track control.key) {
         <div>
           <label for="control.key">{{ control.key }}</label>
-          <input type="text" [id]="control.key" [formControlName]="control.key">
+          <input
+            type="text"
+            [id]="control.key"
+            [formControlName]="control.key"
+          />
         </div>
-      }
-      <button type="submit">Save</button>
-    </form>
-  </section>
+        }
+        <button type="submit">Save</button>
+      </form>
+    </section>
   `,
-  styles: ``
+  styles: ``,
 })
 export class FormModalComponent {
-  @Input({ required: true }) formGroup!: FormGroup
+  @Input({ required: true }) formGroup!: FormGroup;
   @Input({ required: true }) title!: string;
   @Output() save = new EventEmitter<void>();
   @Output() close = new EventEmitter<void>();
