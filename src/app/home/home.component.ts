4b4cf8f8a01ae206bdf0af4dd656da80688168ae
amiloszewicz <miloszewicz.adam@gmail.com>
2024-01-18 22:23:15 +0100

    feat component: checklist-list

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,16 +4,22 @@ import { ChecklistService } from '../shared/data-access/checklist.service';
 import { Checklist } from '../shared/interfaces/checklist';
 import { FormModalComponent } from '../shared/ui/form-modal/form-modal.component';
 import { ModalComponent } from '../shared/ui/modal/modal.component';
+import { ChecklistListComponent } from './ui/checklist-list/checklist-list.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ModalComponent, FormModalComponent],
+  imports: [ModalComponent, FormModalComponent, ChecklistListComponent],
   template: `
     <header>
       <h1>Quicklists</h1>
       <button (click)="checklistBeingEdited.set({})">Add Checklist</button>
     </header>
+
+    <section>
+      <h2>Your checklist</h2>
+      <app-checklist-list [checklists]="checklistService.checklists()" />
+    </section>
 
     <app-modal [isOpen]="!!checklistBeingEdited()">
       <ng-template>
