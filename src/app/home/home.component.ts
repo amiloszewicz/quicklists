2e1372d4c3ae371a89abb5b037b66d0d282c263a
amiloszewicz <miloszewicz.adam@gmail.com>
2024-01-19 14:54:48 +0100

    refactor: change import path to components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,9 +2,9 @@ import { Component, effect, inject, signal } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { ChecklistService } from '../shared/data-access/checklist.service';
 import { Checklist } from '../shared/interfaces/checklist';
-import { FormModalComponent } from '../shared/ui/form-modal/form-modal.component';
-import { ModalComponent } from '../shared/ui/modal/modal.component';
-import { ChecklistListComponent } from './ui/checklist-list/checklist-list.component';
+import { FormModalComponent } from '../shared/ui/form-modal.component';
+import { ModalComponent } from '../shared/ui/modal.component';
+import { ChecklistListComponent } from './ui/checklist-list.component';
 
 @Component({
   selector: 'app-home',
