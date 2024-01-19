b8b0ae4d9c4a75c437691c27529920e170037f82
amiloszewicz <miloszewicz.adam@gmail.com>
2024-01-19 14:42:01 +0100

    refactor path: checklist-header

--- src/app/checklist/ui/checklist-header.component.ts
+++ src/app/checklist/ui/checklist-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Checklist } from '../../../shared/interfaces/checklist';
 
@@ -10,10 +10,14 @@ import { Checklist } from '../../../shared/interfaces/checklist';
     <header>
       <a routerLink="/home">back</a>
       <h1>{{ checklist.title }}</h1>
+      <div>
+        <button (click)="addItem.emit()">Add item</button>
+      </div>
     </header>
   `,
   styles: ``,
 })
 export class ChecklistHeaderComponent {
   @Input({ required: true }) checklist!: Checklist;
+  @Output() addItem = new EventEmitter<void>();
 }
